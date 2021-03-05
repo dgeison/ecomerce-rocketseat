e1a06fda47325ecea866db9316a4627e688833de
Dgeison <paradg@gmail.com>
2021-03-04 23:29:39 -0300

    Função chamando a si mesma?

--- public/scripts.js
+++ public/scripts.js
@@ -1,15 +1,31 @@
-const input = document.querySelector('input[name="price"');
-input.addEventListener("keydown", function (e) {
-  setTimeout(function () {
-    let { value } = e.target;
+// const input = document.querySelector('input[name="price"');
+// input.addEventListener("keydown", function (e) {
+//   setTimeout(function () {
+//     let { value } = e.target;
 
+//     value = value.replace(/\D/g, "");
+
+//     value = new Intl.NumberFormat('pt-BR', {
+//         style: 'currency',
+//         currency: 'BRL'
+//     }).format(value/100)
+
+//     e.target.value = value;
+//   }, 1);
+// });
+
+const Mask = {
+  aply() {
+    setTimeout(function () {
+        input.value = Mask[func](input.value)
+    },1);
+  },
+  formatBRL(value) {
     value = value.replace(/\D/g, "");
 
-    value = new Intl.NumberFormat('pt-BR', {
-        style: 'currency',
-        currency: 'BRL'
-    }).format(value/100)
-
-    e.target.value = value;
-  }, 1);
-});
+    return new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    }).format(value / 100);
+  },
+};
