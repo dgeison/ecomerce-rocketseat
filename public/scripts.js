db77aa49e5c2b0eff9eb9f06b681bb87c8f059f2
Dgeison <paradg@gmail.com>
2021-03-04 23:19:19 -0300

    form aceita somente número

---
+++ public/scripts.js
@@ -0,0 +1,10 @@
+const input = document.querySelector('input[name="price"');
+input.addEventListener("keydown", function (e) {
+  setTimeout(function () {
+    let { value } = e.target;
+
+    value = value.replace(/\D/g, "");
+
+    e.target.value = value;
+  }, 1);
+});
