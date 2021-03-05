37bf9924f1412f0a3232ad2c9817317ad9796f18
Dgeison <paradg@gmail.com>
2021-03-05 16:37:45 -0300

    insert, update, delete and edit

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,12 @@ const ProductController = require("./app/controllers/ProductController");
 routes.get("/", function (req, res) {});
 
 routes.get("/products/create", ProductController.create);
+routes.get("/products/:id/edit", ProductController.edit);
+
+
 routes.post("/products", ProductController.post);
+routes.put("/products", ProductController.put);
+routes.delete("/products", ProductController.delete);
 
 // ALIAS
 routes.get("/ads/create", function (req, res) {
